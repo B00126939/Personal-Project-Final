debf68439b7a110120d4ce167a1a0781e3313d50
B00126939 <71830313+B00126939@users.noreply.github.com>
2020-12-17 03:05:31 +0000

    Unity project added

---
+++ Promise E - Personal Project Final/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    public float horizontalInput;
+    private Rigidbody playerRb;
+    public float xRange = 16.0f;
+    public float jumpForce = 300;
+    public bool isOnGround = true;
+
+    public GameObject projectilePrefab;
+    private float reloadRate = 1f; // time player should wait
+    private float shootRate = 0; // time player can shoot after waiting
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+        ConstraintPlayer();
+        ProjectileLaunch();
+    }
+
+    void Movement()
+    {
+    horizontalInput = Input.GetAxis("Horizontal");
+    transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+
+    void ConstraintPlayer()
+    {
+                // inbounds
+            if (transform.position.x < -xRange)
+            {
+               transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+		    }
+            if (transform.position.x > xRange)
+            {
+               transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+		    }
+
+            if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            {
+               isOnGround = false;
+               playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);                 
+	        }
+
+            
+     }
+
+     void ProjectileLaunch()
+     {
+             if (Input.GetKeyDown(KeyCode.E)&& Time.time > shootRate )
+            {
+               shootRate = Time.time + reloadRate;  // reset shootRate to current time + reloadRate
+               Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);               
+	        }
+	 }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+      
+
+      if (collision.gameObject.CompareTag("Ground"))
+      {
+        isOnGround = true;
+	  }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+    
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy.");
+	    }
+
+        if(other.gameObject.CompareTag("PowerUp"))
+        {
+            Destroy(other.gameObject);
+		}
+
+	}
+}
---
+++ Promise E - Personal Project Final/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+
+    public GameObject projectilePrefab;
+
+    private float projectileSpawnTime = 4.0f;
+    private float startDelay = 1.0f;
+    //Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        InvokeRepeating("ProjectileLaunch", startDelay, projectileSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+ 
+            if(collision.gameObject.CompareTag("Orb"))
+            {
+                Debug.Log("Enemy Collided with Orb");
+	        }
+
+	}
+
+    void ProjectileLaunch()
+    {             
+             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);               
+	}
+}
